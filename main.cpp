754a061fe0a8003454f295a2a45e0d2d43db7857
vasilenokivan <vasilenok.ivan@gmail.com>
2018-04-09 18:16:33 +0300

    Сделал проще парсер командной строки.

--- main.cpp
+++ main.cpp
@@ -5,23 +5,14 @@
 
 int main(int argc, char *argv[])
 {
-    QCommandLineParser parser;
-
     QApplication a(argc, argv);
-
-    QCommandLineOption commandOption("process1", "Start program", "Argument");
-    QCommandLineOption commandOption1("process2", "Start program", "Argument");
-    parser.addOption(commandOption);
-    parser.addOption(commandOption1);
-    parser.process(a);
-    QStringList setStrListCommand;   
-    setStrListCommand.append(parser.value(commandOption));
-    setStrListCommand.append(parser.value(commandOption1));
-
-
+    int findArgc=argc;
+    QStringList findStrListArgv;
+    for(int i=1;i<findArgc;i++){
+        findStrListArgv.append(argv[i]);}
     MainWindow w;
     w.setWindowTitle("AutAgregator");
-    w.setStartArgument(setStrListCommand);
+    w.setStartArgument(findStrListArgv);
     w.show();
 
     return a.exec();
